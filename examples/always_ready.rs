3e9f50b2d34c9cb22b9e531f9f7ce736e1ab684e
Dan Nixon <dan@dan-nixon.com>
2022-08-30 17:26:33 +0100

    Ensure AlwaysReady works for Watcher

---
+++ examples/always_ready.rs
@@ -0,0 +1,64 @@
+use anyhow::Result;
+use kagiyama::{AlwaysReady, Watcher};
+use prometheus_client::metrics::counter::Counter;
+use tokio::time::{self, Duration};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let mut watcher = Watcher::<AlwaysReady>::default();
+
+    watcher.start_server("127.0.0.1:9090".parse()?).await?;
+
+    // Simulate a metric
+    {
+        let watcher = watcher.clone();
+        tokio::spawn(async move {
+            let mut interval = time::interval(Duration::from_secs(1));
+
+            let ticks = Counter::<u64>::default();
+
+            {
+                let mut registry = watcher.metrics_registry();
+                registry.register(
+                    "ticks",
+                    "A demo metric, counts up every second",
+                    Box::new(ticks.clone()),
+                );
+            }
+
+            loop {
+                interval.tick().await;
+                ticks.inc();
+            }
+        });
+    }
+
+    // Simulate a metric
+    {
+        let watcher = watcher.clone();
+        tokio::spawn(async move {
+            let mut interval = time::interval(Duration::from_secs(2));
+
+            let ticks = Counter::<u64>::default();
+
+            {
+                let mut registry = watcher.metrics_registry();
+                let registry = registry.sub_registry_with_prefix("extra_things");
+                registry.register(
+                    "ticks",
+                    "A demo metric, counts up every two seconds",
+                    Box::new(ticks.clone()),
+                );
+            }
+
+            loop {
+                interval.tick().await;
+                ticks.inc();
+            }
+        });
+    }
+
+    time::sleep(Duration::from_secs(300)).await;
+
+    Ok(())
+}
